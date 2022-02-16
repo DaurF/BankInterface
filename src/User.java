b631d76b02199738bb23018ab3255b67ddb47ac9
DaurF <213053@astanait.edu.kz>
2022-02-16 14:44:52 +0600

    Added lastName variable

--- src/User.java
+++ src/User.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
 public class User {
-    private String userName;
-    private String lastName;
+    private String userName; // The First Name
+    private String lastName; // The Last Name
+    private String uuid; // ID
+    private byte pinHash[]; // User's pin number
+    private ArrayList<Account> accounts; // The list of accounts of the user
 }
