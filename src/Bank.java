a939db67eeaab483cde5c13569266f823412a7b5
Dauren <213053@astanait.edu.kz>
2022-02-21 03:56:46 +0600

    Some Updates

--- src/Bank.java
+++ src/Bank.java
@@ -9,6 +9,7 @@ public class Bank
 
     private ArrayList<Account> accounts;
 
+
     /**
      * Create a new Bank object with empty lists of users and accounts
      * @param name the name of the Bank
@@ -24,6 +25,7 @@ public class Bank
      */
     public String getNewUserUUID()
     {
+        // inits
         String uuid;
         Random rng = new Random();
         int len = 6;
@@ -42,28 +44,99 @@ public class Bank
             nonUnique = false;
             for(User u : this.users)
             {
-                if(uuid.compareTo(u.getUUID()) == 0;)
+                if(uuid.compareTo(u.getUUID()) == 0)
                 {
                     nonUnique = true;
                     break;
                 }
             }
-        } while(nonUnique)
+        } while(nonUnique);
 
         return uuid;
 
     }
 
-    public String getNewAccountUUID()
-    {
+    public String getNewAccountUUID() {
+        // inits
+        String uuid;
+        Random rng = new Random();
+        int len = 10;
+        boolean nonUnique;
 
+        // continue looping until we get a unique ID
+        do {
+            // generate the number
+            uuid = "";
+            for(int i = 0; i < len; i++)
+            {
+                uuid += ((Integer)rng.nextInt(10)).toString();
+            }
+
+            // check to be sure it is unique
+            nonUnique = false;
+            for(Account a : this.accounts)
+            {
+                if(uuid.compareTo(a.getUUID()) == 0)
+                {
+                    nonUnique = true;
+                    break;
+                }
+            }
+        } while(nonUnique);
+
+        return uuid;
+    };
+
+    /**
+     * Create a new user of the bank
+     * @param firstName the user's first name
+     * @param lastName  the user's last name
+     * @param pin       the user's pin
+     * @return          the new User object
+     */
+    public User addUser(String firstName, String lastName, String pin)
+    {
+        // create a new User object and add it to our list
+        User newUser = new User(firstName, lastName, pin, this);
+        this.users.add(newUser);
+
+        // create a savings account for the user and add to User and Bank
+        // account lists
+        Account newAccount = new Account("Savings", newUser, this);
+        newUser.addAccount(newAccount);
+        this.accounts.add(newAccount);
+
+        return newUser;
     }
 
     /**
-     * Add an account
-     * @param anAcct    the account to add
+     * Get the User object associated with a particular userID and pin, if they are valid
+     * @param userID    the UUID of the user to log in
+     * @param pin       the pin of the user
+     * @return          the User is object, if the login is successful, or null, if it isn't
      */
-    public void addAccount(Account anAcct){
-        this.accounts.add(anAcct);
+    public User userLogin(String userID, String pin)
+    {
+        // search through list of users
+        for (User u : this.users)
+        {
+            // check user ID is correct
+            if(u.getUUID().compareTo(userID) == 0 && u.validatePin(pin))
+            {
+                return u;
+            }
+        }
+
+        // if we haven't found the user or have an incorrect pin
+        return null;
+    }
+
+    /**
+     * Get the name of the bank
+     * @return the name of the bank
+     */
+    public String getName()
+    {
+        return this.name;
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,6 +1,7 @@
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.security.MessageDigest;
+
 public class User {
     private String firstName; // The First Name
     private String lastName; // The Last Name
@@ -10,53 +11,137 @@ public class User {
 
     /**
      * Create a new user
+     *
      * @param firstName the user's first name
-     * @param lastName the user's last name
-     * @param pin the user's account pin number
-     * @param bank the bank object that the user is a customer of
+     * @param lastName  the user's last name
+     * @param pin       the user's account pin number
+     * @param theBank   the bank object that the user is a customer of
      */
-
-    public User(String firstName, String lastName, String pin, Bank bank) // Setter method
+    public User(String firstName, String lastName, String pin, Bank theBank) // Setter method
     {
+        // set user's name
         this.firstName = firstName;
         this.lastName = lastName;
 
-        try
-        {
+        // store the pin's MD5 hash, rather than the original value, for
+        // security reasons
+        try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             this.pinHash = md.digest(pin.getBytes());
-        } catch (NoSuchAlgorithmException e)
-        {
+        } catch (NoSuchAlgorithmException e) {
             System.err.println("error, caught NoSuchAlgorithmException");
             e.printStackTrace();
             System.exit(1);
         }
 
         // get a unique ID for the user
-        this.uuid = Bank.getNewUserUUID();
+        this.uuid = theBank.getNewUserUUID();
 
         // create empty list of accounts
         this.accounts = new ArrayList<Account>();
 
         // print log message
-        System.out.println("New user %s, %s with ID %s created.\n", lastName, firstName, this.uuid);
+        System.out.printf("New user %s, %s with ID %s created.\n", lastName, firstName, this.uuid);
     }
 
     /**
      * Add an account for the user
-     * @param anAcct    the account to add
+     *
+     * @param anAcct the account to add
      */
-    public void addAccount(Account anAcct)
-    {
+    public void addAccount(Account anAcct) {
         this.accounts.add(anAcct);
     }
 
     /**
      * Return the user's UUID
-     * @return  the uuid
+     *
+     * @return the uuid
      */
-    public String getUUID()
-    {
+    public String getUUID() {
         return this.uuid;
+    }
+
+    /**
+     * Check whether a given pin matches the true User pin
+     *
+     * @param aPin the pin to check
+     * @return whether the pin is valid or not
+     */
+    public boolean validatePin(String aPin) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return MessageDigest.isEqual(md.digest(aPin.getBytes()), this.pinHash);
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("error, caught NoSuchAlgorithmException");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        return false;
+    }
+
+    /**
+     * Return the user's first name
+     *
+     * @return the first name
+     */
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    /**
+     * Print summaries for the accounts of this user.
+     */
+    public void printAccountsSummary() {
+        System.out.printf("\n\n%s's accounts summary\n", this.firstName);
+        for (int i = 0; i < this.accounts.size(); i++) {
+            System.out.printf("  %d) %s\n", i + 1, this.accounts.get(i).getSummaryLine());
+        }
+        System.out.println();
+    }
+
+    /**
+     * Get the number of accounts of the user
+     * @return  the number of accounts
+     */
+    public int numAccounts()
+    {
+        return this.accounts.size();
+    }
+
+    /**
+     * Print transaction history for a particular account.
+     * @param acctIdx   the index of the account to use
+     */
+    public void printAcctTransHistory(int acctIdx)
+    {
+        this.accounts.get(acctIdx).printTransHistory();
+    }
+
+    /**
+     * Get the balance of a particular account
+     * @param acctIdx   the index of the account to use
+     * @return          the balance of the account
+     */
+    public double getAcctBalance(int acctIdx)
+    {
+        return this.accounts.get(acctIdx).getBalance();
+    }
+
+    /**
+     * Get the UUID of a particular account
+     * @param acctIdx   the index of the account to use
+     * @return          the UUID of the account
+     */
+    public String getAccountUUID(int acctIdx)
+    {
+        return this.accounts.get(acctIdx).getUUID();
+    }
+
+    public void addAcctTransaction(int acctIdx, double amount, String memo)
+    {
+        this.accounts.get(acctIdx).addTransaction(amount, memo);
     }
 }
